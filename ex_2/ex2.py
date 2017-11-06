f3a78df9cea55b78f8bc6a951ba2108f48477850
harishschandra <31711508+harishschandra@users.noreply.github.com>
2017-11-06 12:32:51 -0500

    Update ex2.py

--- ex_2/ex2.py
+++ ex_2/ex2.py
@@ -2,7 +2,7 @@ import cv2
 
 pic = cv2.imread('C:/Users/harish/pic_1.png') 
 
-[b,g,r] = cv2.split(pic) #split into color channels
+[b,g,r] = cv2.split(pic) 
 
 cv2.imshow('Blue',b)
 cv2.imwrite('Blue.png',b)
@@ -11,9 +11,9 @@ cv2.imwrite('Red.png',r)
 cv2.imshow('Green',g)
 cv2.imwrite('Green.png',g)
 
-hsv_pic = cv2.cvtColor(pic, cv2.COLOR_BGR2HSV) #convert to HSV
+hsv_pic = cv2.cvtColor(pic, cv2.COLOR_BGR2HSV) 
 
-[h,s,v] = cv2.split(hsv_pic) #split into HSV channells
+[h,s,v] = cv2.split(hsv_pic) 
 
 cv2.imshow('Hue',h)
 cv2.imwrite('Hue.png',h)
@@ -22,9 +22,9 @@ cv2.imwrite('Saturation.png',s)
 cv2.imshow('Value',v)
 cv2.imwrite('Value.png',v)
 
-YCC_pic = cv2.cvtColor(pic, cv2.COLOR_BGR2YCR_CB) #convert to YCrCb
+YCC_pic = cv2.cvtColor(pic, cv2.COLOR_BGR2YCR_CB) 
 
-[y,Cb,Cr] = cv2.split(YCC_pic) #split YCrCb channels
+[y,Cb,Cr] = cv2.split(YCC_pic) 
 
 cv2.imshow('Y',y)
 cv2.imwrite('Y.png',y)
